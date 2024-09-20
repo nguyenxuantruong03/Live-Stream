63ac7d727672a20897a3a04f3cc5b92015198c5e
Xuan Truong <nxt159753@gmail.com>
2024-09-20 12:15:01 +0700

    Clerk webhook

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,7 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { authMiddleware } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
-
-export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
-    auth().protect()
-  }
+export default authMiddleware({
+  publicRoutes: ["/api/webhooks(.*)"]
 })
 
 export const config = {
