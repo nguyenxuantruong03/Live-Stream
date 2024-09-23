8892dcdbc7bedec0e3079171c29e73fd741fd458
Xuan Truong <nxt159753@gmail.com>
2024-09-23 14:58:26 +0700

    Viewer token

--- app/(dashboard)/u/[username]/(home)/page.tsx
+++ app/(dashboard)/u/[username]/(home)/page.tsx
@@ -1,11 +1,26 @@
+import { StreamPlayer } from "@/components/stream-player";
+import { getUserByUsername } from "@/lib/user-service";
+import { currentUser } from "@clerk/nextjs/server";
 
-
-const CreatorPage = () => {
-    return ( 
-        <div>
-            Creator page
-        </div>
-     );
+interface CreatorProps {
+  params: {
+    username: string;
+  };
 }
- 
+
+const CreatorPage = async ({ params }: CreatorProps) => {
+  const externalUser = await currentUser();
+  const user = await getUserByUsername(params.username);
+
+  if (!user || user.externalUserId !== externalUser?.id || !user.stream) {
+    throw new Error("Unauthorized");
+  }
+
+  return (
+    <div className="h-full">
+      <StreamPlayer user={user} stream={user.stream} isFollowing />
+    </div>
+  );
+};
+
 export default CreatorPage;
