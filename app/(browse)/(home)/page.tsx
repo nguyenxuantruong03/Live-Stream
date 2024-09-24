622c9afe92741d0e1c660218cfed9f20e3e4255b
Xuan Truong <nxt159753@gmail.com>
2024-09-24 16:21:04 +0700

    Home page

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react";
+import { Results, ResultSkeleton } from "./_components/results";
+
 export default function Page() {
   return (
-    <div className="flex flex-col gap-y-4">
-      <h1>Home</h1>
+    <div className="h-full p-8 max-w-screen-2xl mx-auto">
+      <Suspense fallback={<ResultSkeleton />}>
+        <Results />
+      </Suspense>
     </div>
   );
 }
